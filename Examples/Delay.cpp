721ff73f138930b93852695dcc5d70958635dee8
Jonas <95506842+JonasHellrigl@users.noreply.github.com>
2022-04-25 16:44:02 +0200

    Update Delay.cpp

--- Examples/Delay.cpp
+++ Examples/Delay.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <Delay.h>
 
-Delay delay1 = Delay(500); // DelayValue
+Delay delay1 = Delay();
 void setup()
 {
 }
 
 void loop()
 {
-  if (delay1.Use_Delay())
+  if (delay1.Use_Delay(500))
   {
     // Put in here the Code
   }
